59cc51d189819e19af9060e02f828d44f7678639
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2020-09-11 18:28:55 +0100

    multichannel data

---
+++ dmtr_11.pl
@@ -0,0 +1,23 @@
+ #!/usr/bin/perl
+ use Demeter;
+ use Demeter::Data::MultiChannel;
+
+# code snippet from https://bruceravel.github.io/demeter/documents/DPG/data/mc.html
+
+# Multi-channel data objects can be used to import data from files that contain 
+# multiple columns of data that can be independently processed as μ(E) data. 
+
+# This example shows the use of the Data::MultiChannel object to import the reference channel
+# from a normal XAS data file. Here is an example:
+
+#my $file = 'data/Fe_pyrite.000';
+my $mc = Demeter::Data::MultiChannel->new(file => 'data/Fe_pyrite.000', energy => '$1', );
+
+my $data = $mc->make_data(numerator   => '$2', denominator => '$3',
+                        ln          => 1,    name        => 'Fe2O3' );
+my $ref  = $mc->make_data(numerator   => '$2', denominator => '$4',
+                        ln          => 1,    name        => '  Ref Fe pyrite' );
+ 
+
+$data -> plot('E');
+$ref -> plot('E')
