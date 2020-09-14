0686aeb4403deac0057fb8afb1bd06b0e147bc33
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2020-09-14 11:53:37 +0100

    fix comment

--- dmtr_01.pl
+++ dmtr_01.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
+
 ## first code snippet from https://bruceravel.github.io/demeter/documents/DPG/data/index.html
-## This is the basic code to require demeter
+
+## This is the basic code to use demeter
+
 use Demeter;
